2a7d342e4d7099394ab63329aeb1ae679e9efc8c
Uneees <belabbes1021@gmail.com>
2023-12-18 10:23:19 +0100

    commit 3.0

--- app/src/main/java/com/example/appevo09/Vehicle.kt
+++
@@ -1,9 +0,0 @@
-package com.example.appevo09
-
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-
-class Vehicle ( var ambit: String, var colleccio: String, var nom: String, var numInventari:Int, var llocFabricacio: String, var procedencia: String, var fontEnergia: String,
-                var potencia: String, var formaIngres: String, var fontIngres: String, var multimedia: MutableList<String>, var any: Int, var cicle: Int,
-                var velocitatMax: Int, var velocitatCreuer:Int, var capacitatDeposit: Int, var llongitud: Float, var alcada: Float, var envergadura: Float, var pes: Float,
-                var pesMax: Float, var cilindrada: Float, var  autonomia: Float, var autonomiaEnTemps: Float)
